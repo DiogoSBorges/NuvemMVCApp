3f588ff65d7f8c535020193d706ece1bff898850
Diogo Sousa Borges <diogosousa2266@gmail.com>
2016-09-21 18:27:41 -0400

    First Commit
    My First App Asp Net Core - Basic Example

---
+++ src/NuvemMVCApp/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NuvemMVCApp.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required, Display(Name ="Name")]
+        public string name { get; set; }
+
+        [Required, Display(Name = "Age")]
+        public int age { get; set; }
+
+        [Required, Display(Name = "Birth Date"), DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime birthDate { get; set; }
+    }
+}
